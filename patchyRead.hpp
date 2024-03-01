31d14c0159b1e412ff176684e19f9295262e33bd
dpearson <dpearson1983@gmail.com>
2024-03-01 11:22:49 -0600

    Fix further errors in randoms file name

--- patchyRead.hpp
+++ patchyRead.hpp
@@ -35,16 +35,23 @@ std::vector<galaxy> readPatchyMock(std::string file, cosmology &cosmo, std::vect
 std::vector<galaxy> readPatchyRandoms(std::string file, cosmology &cosmo, std::vector<double> &r_min,
                                     std::vector<double> &r_max) {
     std::ifstream fin(file, std::ios::in);
+    if (!fin) {
+        std::cout << "Error: Randoms file did not open.\n";
+        std::cout << "Check that file " << file << " exists in the current directory.\n";
+    }
     double ra, dec, redshift, mstar, nbar, bias, veto, fiber_col;
     std::vector<galaxy> rans;
+    // long int count = 1;
     while (!fin.eof()) {
         fin >> ra >> dec >> redshift >> nbar >> bias >> veto >> fiber_col;
+        // std::cout << count << " " << redshift << "\r";
+        // count++;
         if (redshift >= 0.4 && redshift < 0.7) {
             double w = (veto*fiber_col)/(1.0 + 10000.0*nbar);
             galaxy ran(ra, dec, redshift, w, nbar);
             ran.astroToCart(cosmo);
             rans.push_back(ran);
-            std::cout << "Adding random point: " << rans.size() << "\n";
+            //std::cout << "Adding random point: " << rans.size() << "\n";
 
             std::vector<double> pos = ran.getCartPos();
             for (int j = 0; j < 3; ++j) {
